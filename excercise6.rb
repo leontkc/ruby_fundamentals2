b2189f5871ea6eebd4765cda58ded7fbf704b427
Leon Chiu <leontkchiu@gmail.com>
2016-06-01 15:24:27 -0400

    part 2 of ex6

--- excercise6.rb
+++ excercise6.rb
@@ -1,5 +1,15 @@
 grocery_list = ["carrots", "toilet paper", "apples", "salmon"]
 
-grocery_list.map do |x|
+def print_list(grocery_list)
+  grocery_list.each do |x|
   puts "* " + x
+  end
 end
+
+print_list(grocery_list)
+
+grocery_list.push("rice")
+
+print_list(grocery_list)
+
+puts grocery_list.length.to_s + " items on the list"
