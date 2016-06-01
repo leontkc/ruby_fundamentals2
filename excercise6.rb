e454024f1c40b4add98bd1e3c8c4b7098dc1531a
Leon Chiu <leontkchiu@gmail.com>
2016-06-01 15:34:28 -0400

    excercise 2 completed

--- excercise6.rb
+++ excercise6.rb
@@ -2,7 +2,7 @@ grocery_list = ["carrots", "toilet paper", "apples", "salmon"]
 
 def print_list(grocery_list)
   grocery_list.each do |x|
-  puts "* " + x
+  puts "* #{x}"
   end
 end
 
@@ -13,3 +13,24 @@ grocery_list.push("rice")
 print_list(grocery_list)
 
 puts grocery_list.length.to_s + " items on the list"
+
+if grocery_list.include?("bananas")
+  puts "you need to pick up bananas"
+else
+  puts "you don't need to pick up bananas"
+end
+
+puts "the second item is #{grocery_list[1]}"
+
+puts "part 5 - sorted list"
+sorted_list = grocery_list.sort
+print_list(sorted_list)
+
+# grocery_list.sort.each do |x|
+#   puts "* #{x}"
+# end
+
+grocery_list.delete("salmon")
+
+puts "part 6"
+print_list(grocery_list)
